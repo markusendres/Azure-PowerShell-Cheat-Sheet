def819b226111817b5cb10fd967a49e0c26d4fa1
quietshade <6eae220d@opayq.com>
2017-07-20 11:11:17 +0100

    Commands to connect to Azure via PowerShell

---
+++ Azure Powershell Cheat Sheet.ps1
@@ -0,0 +1,27 @@
+<# 
+=============================================================================
+Connecting PowerShell Azure
+=============================================================================
+#>
+
+# Log into Azure Resource Manager.
+Login-AzureRmAccount
+
+# Log into Azure Resource Manager and straight into the appropriate subscription.
+Login-AzureRmAccount -TenantId [aaaa1111-aa11-aa11-aa11-aaaaaa111111]
+
+# List all subscriptions you have access to.
+Get-AzureRmSubscription
+
+# Set the subscription to use.
+Select-AzureRmSubscription -SubscriptionName "[subscription name]"
+
+# View the subscription PowerShell is currently running under.
+Get-AzureRmContext
+
+<# 
+=============================================================================
+Working with Virtual Machines
+=============================================================================
+#>
+
