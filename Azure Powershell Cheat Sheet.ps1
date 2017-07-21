ce61fc45dc5c41aac0acf8fd43ba42620b19a868
quietshade <6eae220d@opayq.com>
2017-07-21 12:44:13 +0100

    updated options to show variables
    Instead of using [ ] to describe parts of the commands that can be change, $variable names are used

--- Azure Powershell Cheat Sheet.ps1
+++ Azure Powershell Cheat Sheet.ps1
@@ -8,13 +8,13 @@ Connecting PowerShell Azure
 Login-AzureRmAccount
 
 # Log into Azure Resource Manager and straight into the appropriate subscription.
-Login-AzureRmAccount -TenantId aaaa1111-aa11-aa11-aa11-aaaaaa111111
+Login-AzureRmAccount -TenantId $MyTenantID
 
 # List all subscriptions you have access to.
 Get-AzureRmSubscription
 
 # Set the subscription to use.
-Select-AzureRmSubscription -SubscriptionName "subscription name"
+Select-AzureRmSubscription -SubscriptionName "$SubscriptionName"
 
 # View the subscription PowerShell is currently running under.
 Get-AzureRmContext
@@ -25,3 +25,8 @@ Working with Virtual Machines
 =============================================================================
 #>
 
+# List all VM's in the subscription
+Get-AzureRmVM
+
+# List all VM's in the subscription
+Get-AzureRmVM -ResourceGroupName $myResourceGroup
